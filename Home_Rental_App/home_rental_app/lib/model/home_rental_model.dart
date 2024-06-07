ac05a2cb9a0ebb4318515906cbec2c0b8aea38a3
ChouguleAnkita13 <flutterankita@gmail.com>
2024-06-07 13:08:22 +0530

    model class

---
+++ Home_Rental_App/home_rental_app/lib/model/home_rental_model.dart
@@ -0,0 +1,23 @@
+class HomeRentalModel {
+  final String name;
+  final String image;
+  final String location;
+  final String details;
+  final String address;
+  final int rent;
+  final int noOfBedRooms;
+  final int noOfBathRooms;
+  final int sqFit;
+
+  const HomeRentalModel({
+    required this.name,
+    required this.image,
+    required this.location,
+    required this.details,
+    required this.address,
+    required this.rent,
+    required this.noOfBathRooms,
+    required this.noOfBedRooms,
+    required this.sqFit,
+  });
+}
