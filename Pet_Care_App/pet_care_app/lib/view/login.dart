e666b23d0194f0f05180b69ae3fbf2d27c138bc3
ChouguleAnkita13 <flutterankita@gmail.com>
2024-06-09 18:25:31 +0530

    login screen

--- Pet_Care_App/pet_care_app/lib/view/login.dart
+++ Pet_Care_App/pet_care_app/lib/view/login.dart
@@ -22,9 +22,10 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.only(top: 50, left: 25, right: 25),
+          padding:
+              const EdgeInsets.only(top: 30, left: 25, right: 25, bottom: 10),
           child: Column(
-            children: [            
+            children: [
               Text(
                 "Login",
                 style: GoogleFonts.poppins(
@@ -32,210 +33,202 @@ class _LoginScreenState extends State<LoginScreen> {
                     color: const Color.fromRGBO(245, 146, 69, 1),
                     fontSize: 32),
               ),
-              
-                      Image.asset("assets/images/logo.png"),
-
-             
+              const SizedBox(
+                height: 10,
+              ),
+              Image.asset("assets/images/logo.png"),
               Form(
                   key: _formKey,
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "Email",
+                        style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w500,
+                            color: const Color.fromRGBO(194, 195, 204, 1),
+                            fontSize: 13),
+                      ),
                       Container(
-                        color: const Color.fromRGBO(249, 250, 251, 1),
-                        margin: const EdgeInsets.only(top: 40),
-                        height: 56,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: const Color.fromRGBO(255, 255, 255, 1),
+                          border: Border.all(
+                              color: const Color.fromRGBO(250, 200, 162, 1),
+                              width: 2),
+                        ),
+                        margin: const EdgeInsets.only(top: 5, bottom: 10),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 14, vertical: 8),
+                        height: 40,
                         child: TextFormField(
                           controller: _emailController,
                           keyboardType: TextInputType.emailAddress,
-                          cursorColor: const Color.fromRGBO(34, 31, 31, 0.4),
-                          decoration: InputDecoration(
-                              prefixIcon: const Icon(
-                                Icons.email_outlined,
-                                size: 24,
-                                color: Color.fromRGBO(34, 31, 31, 0.4),
-                              ),
-                              hintText: "  Enter your email",
-                              hintStyle: GoogleFonts.poppins(
-                                  fontWeight: FontWeight.w400,
-                                  color: const Color.fromRGBO(34, 31, 31, 0.4),
-                                  fontSize: 14),
-                              enabledBorder: const OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                color: Color.fromRGBO(34, 31, 31, 0.1),
-                              )),
-                              focusedBorder: const OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                color: Color.fromRGBO(34, 31, 31, 0.1),
-                              ))),
+                          cursorColor: const Color.fromRGBO(250, 200, 162, 1),
+                          style: GoogleFonts.poppins(
+                              fontWeight: FontWeight.w400,
+                              color: const Color.fromRGBO(194, 195, 204, 1),
+                              fontSize: 12),
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                          ),
                         ),
+                      ),
+                      Text(
+                        "Label",
+                        style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w500,
+                            color: const Color.fromRGBO(194, 195, 204, 1),
+                            fontSize: 13),
                       ),
                       Container(
-                        color: const Color.fromRGBO(249, 250, 251, 1),
-                        margin: const EdgeInsets.only(top: 25, bottom: 15),
-                        height: 56,
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 14, vertical: 8),
+                        margin: const EdgeInsets.only(top: 5, bottom: 8),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: const Color.fromRGBO(255, 255, 255, 1),
+                          border: Border.all(
+                              color: const Color.fromRGBO(250, 200, 162, 1),
+                              width: 2),
+                        ),
+                        height: 40,
                         child: TextFormField(
                           controller: _passwordController,
-                          cursorColor: const Color.fromRGBO(34, 31, 31, 0.4),
+                          cursorColor: const Color.fromRGBO(250, 200, 162, 1),
                           obscureText: _isPasswordVisiable,
-                          obscuringCharacter: "*",
+                          style: GoogleFonts.poppins(
+                              fontWeight: FontWeight.w400,
+                              color: const Color.fromRGBO(194, 195, 204, 1),
+                              fontSize: 12),
                           decoration: InputDecoration(
-                              prefixIcon: const Icon(
-                                Icons.lock_outline_rounded,
+                            suffixIcon: GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  _isPasswordVisiable = !_isPasswordVisiable;
+                                });
+                              },
+                              child: Icon(
+                                _isPasswordVisiable
+                                    ? Icons.visibility_off_outlined
+                                    : Icons.visibility_outlined,
                                 size: 24,
-                                color: Color.fromRGBO(34, 31, 31, 0.4),
+                                color: const Color.fromRGBO(245, 146, 69, 1),
                               ),
-                              suffixIcon: GestureDetector(
-                                onTap: () {
-                                  setState(() {
-                                    _isPasswordVisiable = !_isPasswordVisiable;
-                                  });
-                                },
-                                child: Icon(
-                                  _isPasswordVisiable
-                                      ? Icons.visibility_off_outlined
-                                      : Icons.visibility_outlined,
-                                  size: 24,
-                                  color: const Color.fromRGBO(34, 31, 31, 0.4),
-                                ),
-                              ),
-                              hintText: "  Enter your password",
-                              hintStyle: GoogleFonts.poppins(
-                                  fontWeight: FontWeight.w400,
-                                  color: const Color.fromRGBO(34, 31, 31, 0.4),
-                                  fontSize: 14),
-                              enabledBorder: const OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                color: Color.fromRGBO(34, 31, 31, 0.1),
-                              )),
-                              focusedBorder: const OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                color: Color.fromRGBO(34, 31, 31, 0.1),
-                              ))),
+                            ),
+                            border: InputBorder.none,
+                          ),
                         ),
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text(
-                            "Forgot password?",
+                      Center(
+                        child: Text.rich(TextSpan(children: [
+                          TextSpan(
+                            text: "Forgot Password ?",
                             style: GoogleFonts.poppins(
-                                fontWeight: FontWeight.w500,
-                                color: const Color.fromRGBO(64, 124, 226, 1),
+                                fontWeight: FontWeight.w400,
+                                color: const Color.fromRGBO(31, 32, 41, 1),
                                 fontSize: 13),
                           ),
-                        ],
+                          WidgetSpan(
+                              child: GestureDetector(
+                            onTap: () {},
+                            child: Text(
+                              "Click Here",
+                              style: GoogleFonts.poppins(
+                                  fontWeight: FontWeight.w500,
+                                  color: const Color.fromRGBO(31, 32, 41, 1),
+                                  fontSize: 13),
+                            ),
+                          ))
+                        ])),
                       ),
                       const SizedBox(
-                        height: 50,
+                        height: 20,
                       ),
                       ElevatedButton(
                         onPressed: () {
                           Navigator.of(context).pushNamed("/home");
                         },
                         style: ElevatedButton.styleFrom(
-                          fixedSize: Size(MediaQuery.sizeOf(context).width, 56),
+                          fixedSize: Size(MediaQuery.sizeOf(context).width, 38),
                           backgroundColor:
-                              const Color.fromRGBO(64, 124, 226, 1),
+                              const Color.fromRGBO(245, 146, 69, 1),
                           shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(32),
-                            side: const BorderSide(
-                              color: Color.fromRGBO(64, 124, 226, 1),
-                            ),
+                            borderRadius: BorderRadius.circular(8),
                           ),
                         ),
                         child: Text(
-                          "Sign In",
+                          "LOGIN",
                           style: GoogleFonts.poppins(
                               fontWeight: FontWeight.w600,
                               color: const Color.fromRGBO(255, 255, 255, 1),
-                              fontSize: 16),
+                              fontSize: 15),
                         ),
                       ),
-                      const SizedBox(height: 10),
-                      Text.rich(TextSpan(children: [
-                        TextSpan(
-                          text: "Don’t have an account?",
-                          style: GoogleFonts.poppins(
-                              fontWeight: FontWeight.w400,
-                              color: const Color.fromRGBO(34, 31, 31, 1),
-                              fontSize: 14),
-                        ),
-                        WidgetSpan(
-                            child: GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).pushNamed("/signup");
-                          },
-                          child: Text(
-                            "  Sign up",
-                            style: GoogleFonts.poppins(
-                                fontWeight: FontWeight.w600,
-                                color: const Color.fromRGBO(64, 124, 226, 1),
-                                fontSize: 14),
-                          ),
-                        ))
-                      ])),
                     ],
                   )),
-              const SizedBox(height: 60),
-              Center(
+              Container(
+                margin: const EdgeInsets.only(top: 25, bottom: 25),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(
+                    color: const Color.fromRGBO(245, 146, 69, 1),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  height: 38,
+                  width: MediaQuery.sizeOf(context).width,
+                  alignment: Alignment.center,
+                  margin: const EdgeInsets.only(bottom: 15),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: const Color.fromRGBO(245, 146, 69, 1),
+                  ),
+                  child: Text(
+                    "LOGIN WITH EMAIL",
+                    style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w600,
+                        color: const Color.fromRGBO(255, 255, 255, 1),
+                        fontSize: 14),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  height: 38,
+                  margin: const EdgeInsets.only(bottom: 40),
+                  width: MediaQuery.sizeOf(context).width,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: const Color.fromRGBO(245, 146, 69, 1),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Text(
+                    "LOGIN WITH FACEBOOK",
+                    style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w600,
+                        color: const Color.fromRGBO(255, 255, 255, 1),
+                        fontSize: 16),
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.bottomCenter,
+                width: 200,
                 child: Text(
-                  "OR",
+                  "By continue you agree to our Terms & Privacy Policy",
+                  textAlign: TextAlign.center,
                   style: GoogleFonts.poppins(
                       fontWeight: FontWeight.w400,
-                      color: const Color.fromRGBO(161, 168, 176, 1),
-                      fontSize: 16),
-                ),
-              ),
-              GestureDetector(
-                onTap: () {},
-                child: Container(
-                  height: 56,
-                  margin: const EdgeInsets.only(top: 25),
-                  padding: const EdgeInsets.only(left: 20),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(6),
-                      color: const Color.fromRGBO(255, 255, 255, 1),
-                      border: Border.all(
-                        color: const Color.fromRGBO(229, 231, 235, 1),
-                      )),
-                  child: Row(
-                    children: [
-                      const SizedBox(width: 20),
-                      Text(
-                        "Sign in with Google",
-                        style: GoogleFonts.poppins(
-                            fontWeight: FontWeight.w600,
-                            color: const Color.fromRGBO(34, 31, 31, 1),
-                            fontSize: 16),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              GestureDetector(
-                onTap: () {},
-                child: Container(
-                  height: 56,
-                  margin: const EdgeInsets.only(top: 25),
-                  padding: const EdgeInsets.only(left: 20),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(6),
-                      color: const Color.fromRGBO(255, 255, 255, 1),
-                      border: Border.all(
-                        color: const Color.fromRGBO(229, 231, 235, 1),
-                      )),
-                  child: Row(
-                    children: [
-                      const SizedBox(width: 20),
-                      Text(
-                        "Sign in with Facebook",
-                        style: GoogleFonts.poppins(
-                            fontWeight: FontWeight.w600,
-                            color: const Color.fromRGBO(34, 31, 31, 1),
-                            fontSize: 16),
-                      ),
-                    ],
-                  ),
+                      color: const Color.fromRGBO(31, 32, 41, 1),
+                      fontSize: 13),
                 ),
               ),
             ],
