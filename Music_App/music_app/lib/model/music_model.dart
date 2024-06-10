74e2f1ec0865f8bfcef749eb93284de511473bc0
ChouguleAnkita13 <flutterankita@gmail.com>
2024-06-10 20:41:47 +0530

    create model class

---
+++ Music_App/music_app/lib/model/music_model.dart
@@ -0,0 +1,15 @@
+class MusicModel{
+  final String name;
+  final String img;
+  final int year;
+  final String type;
+  final int time;
+
+  const MusicModel({
+    required this.name,
+    required this.img,
+    required this.year,
+    required this.type,
+    required this.time
+  });
+}
