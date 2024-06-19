af820496c5a61c7c4794a85b4fb06dbb4e998b41
ChouguleAnkita13 <flutterankita@gmail.com>
2024-06-19 22:41:30 +0530

    commit

---
+++ Bicycle_App/bicycle_app/lib/view/screens/get_start.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class GetStart extends StatelessWidget {
+  const GetStart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [
+                  Color.fromRGBO(53, 63, 84, 1),
+                  Color.fromRGBO(75, 76, 237, 1),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                stops: [0.5, 0.5]),),
+         
+      ),
+
+    );
+  }
+}
