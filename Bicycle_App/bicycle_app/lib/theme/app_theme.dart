81bbd0bf8a1cedbcc2cdaea1850a77fb84315942
ChouguleAnkita13 <flutterankita@gmail.com>
2024-06-26 21:39:52 +0530

    added theme

---
+++ Bicycle_App/bicycle_app/lib/theme/app_theme.dart
@@ -0,0 +1,70 @@
+// app_theme.dart
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  static TextStyle poppins({
+    FontWeight fontWeight = FontWeight.w400,
+    Color color = const Color.fromRGBO(34, 31, 31, 1),
+    double fontSize = 14,
+  }) {
+    return GoogleFonts.poppins(
+      fontWeight: fontWeight,
+      color: color,
+      fontSize: fontSize,
+    );
+  }
+
+  static const Color primaryColor = Color.fromRGBO(53, 63, 84, 1);
+  static const Color primaryLightColor = Color.fromRGBO(75, 76, 237, 1);
+  static const Color textColor = Color.fromRGBO(255, 255, 255, 1);
+  static const Color textColorLight = Color.fromRGBO(195, 195, 195, 1);
+  static const Color iconColor = Color.fromRGBO(240, 241, 250, 1);
+
+  static const LinearGradient primaryGradient = LinearGradient(
+    colors: [primaryColor, primaryLightColor],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    stops: [0.5, 0.5],
+  );
+  static const LinearGradient secondaryGradient = LinearGradient(
+    colors: [Color.fromRGBO(53, 63, 84, 0.6), Color.fromRGBO(34, 40, 52, 0.6)],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    stops: [0.5, 0.5],
+  );
+  static const LinearGradient ternaryGradient = LinearGradient(
+    colors: [
+      Color.fromRGBO(55, 182, 233, 1),
+      Color.fromRGBO(72, 92, 236, 1),
+      Color.fromRGBO(75, 76, 237, 1)
+    ],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    stops: [0.3,0.2, 0.3],
+  );
+
+  static final ThemeData lightTheme = ThemeData(
+    primaryColor: primaryColor,
+    textTheme: TextTheme(
+      displayLarge: GoogleFonts.allertaStencil(
+          fontWeight: FontWeight.w400,
+          fontSize: 150,
+          color: const Color.fromRGBO(255, 255, 255, 0.3),),
+      titleLarge:
+          poppins(fontWeight: FontWeight.w500, fontSize: 30, color: textColor),
+      titleMedium:
+          poppins(fontWeight: FontWeight.w500, fontSize: 20, color: textColor),
+      bodyLarge: poppins(
+          fontWeight: FontWeight.w500, fontSize: 15, color: textColorLight),
+      labelLarge: poppins(
+          fontWeight: FontWeight.w600, fontSize: 21, color: Colors.white),
+      bodySmall: poppins(
+          fontWeight: FontWeight.w500, fontSize: 13, color: primaryColor),
+    ),
+    iconTheme: const IconThemeData(
+      size: 30,
+      color: textColor
+    )
+  );
+}
