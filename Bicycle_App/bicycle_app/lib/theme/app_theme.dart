6f8958c2e17325ddac65b538710b3ab467f0d9f6
ChouguleAnkita13 <flutterankita@gmail.com>
2024-06-27 00:24:20 +0530

    added

--- Bicycle_App/bicycle_app/lib/theme/app_theme.dart
+++ Bicycle_App/bicycle_app/lib/theme/app_theme.dart
@@ -27,44 +27,48 @@ class AppTheme {
     end: Alignment.bottomRight,
     stops: [0.5, 0.5],
   );
-  static const LinearGradient secondaryGradient = LinearGradient(
-    colors: [Color.fromRGBO(53, 63, 84, 0.6), Color.fromRGBO(34, 40, 52, 0.6)],
-    begin: Alignment.topLeft,
-    end: Alignment.bottomRight,
-    stops: [0.5, 0.5],
-  );
-  static const LinearGradient ternaryGradient = LinearGradient(
-    colors: [
-      Color.fromRGBO(55, 182, 233, 1),
-      Color.fromRGBO(72, 92, 236, 1),
-      Color.fromRGBO(75, 76, 237, 1)
-    ],
-    begin: Alignment.topLeft,
-    end: Alignment.bottomRight,
-    stops: [0.3,0.2, 0.3],
-  );
+
+  static BoxDecoration contDecoration1 = BoxDecoration(
+      gradient: const LinearGradient(
+        colors: [
+          Color.fromRGBO(53, 63, 84, 0.5),
+          Color.fromRGBO(34, 40, 52, 0.5),
+        ],
+        begin: Alignment.centerLeft,
+        end: Alignment.centerRight,
+      ),
+      borderRadius: BorderRadius.circular(10));
+  static BoxDecoration contDecoration = BoxDecoration(
+      gradient: const LinearGradient(
+        colors: [
+          Color.fromRGBO(72, 92, 236, 1),
+          Color.fromRGBO(75, 76, 237, 1),
+          Color.fromRGBO(55, 182, 233, 1),
+        ],
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        stops: [0.9, 0.0, 0.0],
+      ),
+      borderRadius: BorderRadius.circular(10));
 
   static final ThemeData lightTheme = ThemeData(
-    primaryColor: primaryColor,
-    textTheme: TextTheme(
-      displayLarge: GoogleFonts.allertaStencil(
+      primaryColor: primaryColor,
+      textTheme: TextTheme(
+        displayLarge: GoogleFonts.allertaStencil(
           fontWeight: FontWeight.w400,
           fontSize: 150,
-          color: const Color.fromRGBO(255, 255, 255, 0.3),),
-      titleLarge:
-          poppins(fontWeight: FontWeight.w500, fontSize: 30, color: textColor),
-      titleMedium:
-          poppins(fontWeight: FontWeight.w500, fontSize: 20, color: textColor),
-      bodyLarge: poppins(
-          fontWeight: FontWeight.w500, fontSize: 15, color: textColorLight),
-      labelLarge: poppins(
-          fontWeight: FontWeight.w600, fontSize: 21, color: Colors.white),
-      bodySmall: poppins(
-          fontWeight: FontWeight.w500, fontSize: 13, color: primaryColor),
-    ),
-    iconTheme: const IconThemeData(
-      size: 30,
-      color: textColor
-    )
-  );
+          color: const Color.fromRGBO(255, 255, 255, 0.3),
+        ),
+        titleLarge: poppins(
+            fontWeight: FontWeight.w500, fontSize: 30, color: textColor),
+        titleMedium: poppins(
+            fontWeight: FontWeight.w500, fontSize: 20, color: textColor),
+        bodyLarge: poppins(
+            fontWeight: FontWeight.w500, fontSize: 15, color: textColorLight),
+        labelLarge: poppins(
+            fontWeight: FontWeight.w600, fontSize: 21, color: textColor),
+        bodySmall: poppins(
+            fontWeight: FontWeight.w500, fontSize: 13, color: primaryColor),
+      ),
+      iconTheme: const IconThemeData(size: 30, color: textColor));
 }
