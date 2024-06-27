5c6fb8e64e47b2b62f5fcd1097ca39a4320e92ea
ChouguleAnkita13 <flutterankita@gmail.com>
2024-06-27 23:05:31 +0530

    items added

---
+++ Bicycle_App/bicycle_app/lib/items/itemlists.dart
@@ -0,0 +1,50 @@
+List bicycleItem = [
+  "assets/images/cycle.png",
+  "assets/images/gluffs.png",
+  "assets/images/helmet.png",
+  "assets/images/bottle.png"
+];
+
+List bicycles = [
+  
+  { 'img':"assets/images/kiross.png",
+    'type': 'Road Bike',
+    'name': 'Kiross',
+    'price': 1599.99,
+    'description': '''Lorem ipsum dolor sit amet. Ab tenetur molestias vel 
+                    rerum cupiditate qui dolores consequatur et 
+                    asperiores sunt ea magnam dolorem qui consectetur omnis. Ut error voluptas qui tempora provident 
+                    aut necessitatibus voluptas rem eveniet nulla ut cusantium dignissimos aut facilis perspiciatis anatus quia.'''
+  },
+  
+  { 
+    'img':"assets/images/gt.png",
+    'type': 'Road Bike',
+    'name': 'GT Bike',
+    'price': 2999.99,
+    'description': '''Lorem ipsum dolor sit amet. Ab tenetur molestias vel 
+                    rerum cupiditate qui dolores consequatur et 
+                    asperiores sunt ea magnam dolorem qui consectetur omnis. Ut error voluptas qui tempora provident 
+                    aut necessitatibus voluptas rem eveniet nulla ut cusantium dignissimos aut facilis perspiciatis anatus quia.'''
+  },
+  {
+    'img':"assets/images/scott.png",
+    'type': 'Road Bike',
+    'name': 'Scott',
+    'price': 2399.99,
+    'description': '''Lorem ipsum dolor sit amet. Ab tenetur molestias vel 
+                    rerum cupiditate qui dolores consequatur et 
+                    asperiores sunt ea magnam dolorem qui consectetur omnis. Ut error voluptas qui tempora provident 
+                    aut necessitatibus voluptas rem eveniet nulla ut cusantium dignissimos aut facilis perspiciatis anatus quia.'''
+  },
+  {
+    'img':"assets/images/ross.png",
+    'type': 'Road Bike',
+    'name': 'Ross',
+    'price': 1999.99,
+    'description': '''Lorem ipsum dolor sit amet. Ab tenetur molestias vel 
+                    rerum cupiditate qui dolores consequatur et 
+                    asperiores sunt ea magnam dolorem qui consectetur omnis. Ut error voluptas qui tempora provident 
+                    aut necessitatibus voluptas rem eveniet nulla ut cusantium dignissimos aut facilis perspiciatis anatus quia.'''
+  }
+];
