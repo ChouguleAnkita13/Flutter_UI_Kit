60a5a2635e36b2f822d7eff894d1685709ce683b
ChouguleAnkita13 <flutterankita@gmail.com>
2024-06-28 23:14:01 +0530

    bottom navigation added

---
+++ Bicycle_App/bicycle_app/lib/view/widgets/bottom_navigation.dart
@@ -0,0 +1,27 @@
+import 'package:bicycle_app/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavigation extends StatelessWidget {
+  const BottomNavigation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+        backgroundColor: const Color.fromRGBO(53, 63, 84, 1),
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+              icon: Container(
+                  decoration: AppTheme.smallContDecoration,
+                  height: 0,
+                  child: Image.asset("assets/images/house.png")),
+              label: ""),
+          BottomNavigationBarItem(
+              icon: Image.asset("assets/images/bag.png"), label: ""),
+          BottomNavigationBarItem(
+              icon: Image.asset("assets/images/wallet.png"), label: ""),
+          BottomNavigationBarItem(
+              icon: Image.asset("assets/images/user.png"), label: ""),
+        ]);
+  }
+}
