f2f3a79a02e93f376bc1c441aee0e29cd9f5d47d
ChouguleAnkita13 <flutterankita@gmail.com>
2024-06-29 22:41:49 +0530

    modified

--- Bicycle_App/bicycle_app/lib/items/itemlists.dart
+++ Bicycle_App/bicycle_app/lib/items/itemlists.dart
@@ -6,54 +6,44 @@ List bicycleItem = [
 ];
 
 List bicycles = [
-  
-  { 'img':"assets/images/kiross.png",
+  {
+    'img': "assets/images/kiross.png",
     'type': 'Road Bike',
     'name': 'Kiross',
-    'price': 1599.99,
-    'description': '''Lorem ipsum dolor sit amet. Ab tenetur molestias vel 
-                    rerum cupiditate qui dolores consequatur et 
-                    asperiores sunt ea magnam dolorem qui consectetur omnis. Ut error voluptas qui tempora provident 
-                    aut necessitatibus voluptas rem eveniet nulla ut cusantium dignissimos aut facilis perspiciatis anatus quia.'''
+    'price':"1,599.99",
+    'description':
+        '''Lorem ipsum dolor sit amet. Ab tenetur molestias vel rerum cupiditate qui dolores consequatur et asperiores sunt ea magnam dolorem qui consectetur omnis. Ut error voluptas qui tempora provident aut necessitatibus voluptas rem eveniet nulla ut cusantium dignissimos aut facilis perspiciatis anatus quia.'''
   },
-  
-  { 
-    'img':"assets/images/gt.png",
+  {
+    'img': "assets/images/gt.png",
     'type': 'Road Bike',
     'name': 'GT Bike',
-    'price': 2999.99,
-    'description': '''Lorem ipsum dolor sit amet. Ab tenetur molestias vel 
-                    rerum cupiditate qui dolores consequatur et 
-                    asperiores sunt ea magnam dolorem qui consectetur omnis. Ut error voluptas qui tempora provident 
-                    aut necessitatibus voluptas rem eveniet nulla ut cusantium dignissimos aut facilis perspiciatis anatus quia.'''
+    'price': "2,599.99",
+    'description':
+        '''Lorem ipsum dolor sit amet. Ab tenetur molestias vel rerum cupiditate qui dolores consequatur et asperiores sunt ea magnam dolorem qui consectetur omnis. Ut error voluptas qui tempora provident aut necessitatibus voluptas rem eveniet nulla ut cusantium dignissimos aut facilis perspiciatis anatus quia.'''
   },
   {
-    'img':"assets/images/scott.png",
+    'img': "assets/images/scott.png",
     'type': 'Road Bike',
     'name': 'Scott',
-    'price': 2399.99,
-    'description': '''Lorem ipsum dolor sit amet. Ab tenetur molestias vel 
-                    rerum cupiditate qui dolores consequatur et 
-                    asperiores sunt ea magnam dolorem qui consectetur omnis. Ut error voluptas qui tempora provident 
-                    aut necessitatibus voluptas rem eveniet nulla ut cusantium dignissimos aut facilis perspiciatis anatus quia.'''
+    'price': "2,399.99",
+    'description':
+        '''Lorem ipsum dolor sit amet. Ab tenetur molestias vel rerum cupiditate qui dolores consequatur et asperiores sunt ea magnam dolorem qui consectetur omnis. Ut error voluptas qui tempora provident aut necessitatibus voluptas rem eveniet nulla ut cusantium dignissimos aut facilis perspiciatis anatus quia.'''
   },
   {
-    'img':"assets/images/ross.png",
+    'img': "assets/images/ross.png",
     'type': 'Road Bike',
     'name': 'Ross',
-    'price': 1999.99,
-    'description': '''Lorem ipsum dolor sit amet. Ab tenetur molestias vel 
-                    rerum cupiditate qui dolores consequatur et 
-                    asperiores sunt ea magnam dolorem qui consectetur omnis. Ut error voluptas qui tempora provident 
-                    aut necessitatibus voluptas rem eveniet nulla ut cusantium dignissimos aut facilis perspiciatis anatus quia.'''
-  }, {
-    'img':"assets/images/ross.png",
+    'price': "1,999.99",
+    'description':
+        '''Lorem ipsum dolor sit amet. Ab tenetur molestias vel rerum cupiditate qui dolores consequatur et asperiores sunt ea magnam dolorem qui consectetur omnis. Ut error voluptas qui tempora provident aut necessitatibus voluptas rem eveniet nulla ut cusantium dignissimos aut facilis perspiciatis anatus quia.'''
+  },
+  {
+    'img': "assets/images/ross.png",
     'type': 'Road Bike',
     'name': 'Ross',
-    'price': 1999.99,
-    'description': '''Lorem ipsum dolor sit amet. Ab tenetur molestias vel 
-                    rerum cupiditate qui dolores consequatur et 
-                    asperiores sunt ea magnam dolorem qui consectetur omnis. Ut error voluptas qui tempora provident 
-                    aut necessitatibus voluptas rem eveniet nulla ut cusantium dignissimos aut facilis perspiciatis anatus quia.'''
+    'price': "1,999.99",
+    'description':
+        '''Lorem ipsum dolor sit amet. Ab tenetur molestias vel rerum cupiditate qui dolores consequatur et asperiores sunt ea magnam dolorem qui consectetur omnis. Ut error voluptas qui tempora provident aut necessitatibus voluptas rem eveniet nulla ut cusantium dignissimos aut facilis perspiciatis anatus quia.'''
   }
 ];
