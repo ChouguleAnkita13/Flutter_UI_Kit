aec7c7b4ea0ca4cab97597a5a365be2b619553a2
ChouguleAnkita13 <flutterankita@gmail.com>
2024-07-02 23:37:27 +0530

    shopping_cart done

--- Bicycle_App/bicycle_app/lib/view/screens/shopping_cart.dart
+++ Bicycle_App/bicycle_app/lib/view/screens/shopping_cart.dart
@@ -1,6 +1,9 @@
+import 'package:bicycle_app/view/screens/home_screen.dart';
 import 'package:bicycle_app/view/widgets/cart.dart';
 import 'package:flutter/material.dart';
 import 'package:bicycle_app/theme/app_theme.dart';
+import 'package:intl/intl.dart';
+import '../widgets/total.dart';
 
 class ShoppingCart extends StatelessWidget {
   const ShoppingCart({super.key, required this.bicyleMap});
@@ -73,53 +76,128 @@ class ShoppingCart extends StatelessWidget {
                 ],
               ),
               Positioned(
-                  top: 40,
-                  child: Column(
-                    children: [
-                      Cart(
-                          img: bicyleMap["img"],
-                          name: bicyleMap["name"],
-                          price: bicyleMap["price"]),
-                      Cart(
-                          img: bicyleMap["img"],
-                          name: bicyleMap["name"],
-                          price: bicyleMap["price"]),
-                      Cart(
-                          img: bicyleMap["img"],
-                          name: bicyleMap["name"],
-                          price: bicyleMap["price"]),
-                    ],
-                  ))
+                top: 40,
+                child: Column(
+                  children: [
+                    Cart(
+                        img: bicyleMap["img"],
+                        name: bicyleMap["name"],
+                        price: bicyleMap["price"]),
+                    const Cart(
+                        img: "assets/images/cartHelmet.png",
+                        name: "Helmet",
+                        price: 125.99),
+                    const Cart(
+                        img: "assets/images/cartBottle.png",
+                        name: "Bottle",
+                        price: 115.99),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text("Your Bag Qualifies for Free Shipping",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyLarge!
+                            .copyWith(fontWeight: FontWeight.w300)),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      decoration: AppTheme.contDecoration1
+                          .copyWith(borderRadius: BorderRadius.circular(10)),
+                      height: 50,
+                      width: MediaQuery.of(context).size.width - 30,
+                      alignment: Alignment.center,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("6Affg5",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium!
+                                  .copyWith(
+                                      fontSize: 22,
+                                      color: AppTheme.textColorLight
+                                          .withOpacity(0.7))),
+                          GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                              width: 137,
+                              height: 42,
+                              alignment: Alignment.center,
+                              decoration: AppTheme.smallContDecoration.copyWith(
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: Text("Apply",
+                                  style:
+                                      Theme.of(context).textTheme.titleMedium),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    SingleChildScrollView(
+                      child: SizedBox(
+                        height: 150,
+                        width: MediaQuery.of(context).size.width - 30,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Total(
+                                title: "Subtotal",
+                                value:
+                                    "\$${NumberFormat('#,##0.00', 'en_US').format(bicyleMap["price"] + 125.99 + 115.99)}"),
+                            const Total(title: "Delivery fee", value: "\$0"),
+                            const Total(title: "Discount", value: "30%"),
+                            Total(
+                                title: "Total",
+                                value:
+                                    "\$${NumberFormat('#,##0.00', 'en_US').format((bicyleMap["price"] + 125.99 + 115.99) * 0.7)}"),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
       ),
-      bottomNavigationBar: Container(
-        color: const Color.fromRGBO(0, 0, 0, 0.9),
-        padding: const EdgeInsets.only(bottom: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: 25,
-              height: 25,
-              alignment: Alignment.center,
-              decoration: AppTheme.smallContDecoration
-                  .copyWith(borderRadius: BorderRadius.circular(5)),
-              child: const Icon(
-                Icons.arrow_forward_ios_rounded,
-                size: 21,
+      bottomNavigationBar: GestureDetector(
+        onTap: () {
+          Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => const HomeScreen()));
+        },
+        child: Container(
+          color: const Color.fromRGBO(0, 0, 0, 0.9),
+          padding: const EdgeInsets.only(bottom: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 25,
+                height: 25,
+                alignment: Alignment.center,
+                decoration: AppTheme.smallContDecoration
+                    .copyWith(borderRadius: BorderRadius.circular(5)),
+                child: const Icon(
+                  Icons.arrow_forward_ios_rounded,
+                  size: 21,
+                ),
               ),
-            ),
-            const SizedBox(
-              width: 8,
-            ),
-            Text("Check Out",
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyLarge!
-                    .copyWith(fontSize: 18, color: AppTheme.textColor)),
-          ],
+              const SizedBox(
+                width: 8,
+              ),
+              Text("Check Out",
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyLarge!
+                      .copyWith(fontSize: 18, color: AppTheme.textColor)),
+            ],
+          ),
         ),
       ),
     );
