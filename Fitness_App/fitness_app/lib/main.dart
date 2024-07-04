0cba1db5154e643e7affc687ff8d695927038d6d
ChouguleAnkita13 <flutterankita@gmail.com>
2024-07-04 21:12:35 +0530

    google_fonts

--- Fitness_App/fitness_app/lib/main.dart
+++ Fitness_App/fitness_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fitness_app/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,12 +10,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      // home: 
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme
     );
   }
 }
