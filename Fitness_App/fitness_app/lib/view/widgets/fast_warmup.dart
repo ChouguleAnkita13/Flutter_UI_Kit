1542dafb18f92ad8044132743d1b4b49154c232c
ChouguleAnkita13 <flutterankita@gmail.com>
2024-07-10 22:44:09 +0530

    widget of explore

---
+++ Fitness_App/fitness_app/lib/view/widgets/fast_warmup.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FastWarmUp extends StatelessWidget {
+  const FastWarmUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
