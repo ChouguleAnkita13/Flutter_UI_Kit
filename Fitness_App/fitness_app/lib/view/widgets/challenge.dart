6f433d38dfabd5950ee68b53b152199d84887ec8
ChouguleAnkita13 <flutterankita@gmail.com>
2024-07-10 22:42:41 +0530

    widget of explore

---
+++ Fitness_App/fitness_app/lib/view/widgets/challenge.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Challenge extends StatelessWidget {
+  const Challenge({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
