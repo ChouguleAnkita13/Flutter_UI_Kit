695b4168eef4ecbda5d211925997013420ad6c35
ChouguleAnkita13 <flutterankita@gmail.com>
2024-08-01 18:24:51 +0530

    added model class

---
+++ Fashion_App/fashion_app/lib/model/fashion_model.dart
@@ -0,0 +1,23 @@
+class FashionModel{
+  final String imgUrl;
+  final String fullImgUrl;
+  final String brandName;
+  final double price;
+
+  const FashionModel({required this.imgUrl,required this.fullImgUrl,required this.brandName,required this.price});
+
+}
+
+List fashionItemList=[
+
+  const FashionModel(imgUrl: "assets/images/dummy/shirt1.png",fullImgUrl: "assets/images/shirt1.png", brandName:"Tagerine Shirt", price:240.32),
+  const FashionModel(imgUrl: "assets/images/dummy/coart1.png", fullImgUrl: "assets/images/coart1.png",brandName:"Leather Coart", price:325.36),
+  const FashionModel(imgUrl: "assets/images/dummy/shirt2.png",fullImgUrl: "assets/images/shirt2.png", brandName:"Tagerine Shirt", price:126.47),
+  const FashionModel(imgUrl: "assets/images/dummy/coart2.png",fullImgUrl: "assets/images/coart2.png", brandName:"Leather Coart", price:257.85),
+
+const FashionModel(imgUrl: "assets/images/dummy/shirt1.png",fullImgUrl: "assets/images/shirt1.png", brandName:"Tagerine Shirt", price:240.32),
+  const FashionModel(imgUrl: "assets/images/dummy/coart1.png", fullImgUrl: "assets/images/coart1.png",brandName:"Leather Coart", price:325.36),
+  const FashionModel(imgUrl: "assets/images/dummy/shirt2.png",fullImgUrl: "assets/images/shirt2.png", brandName:"Tagerine Shirt", price:126.47),
+  const FashionModel(imgUrl: "assets/images/dummy/coart2.png",fullImgUrl: "assets/images/coart2.png", brandName:"Leather Coart", price:257.85),
+
+];
