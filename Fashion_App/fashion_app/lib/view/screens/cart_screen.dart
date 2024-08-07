e53f8966a38827a8624bcd80b2065883c44427a7
ChouguleAnkita13 <flutterankita@gmail.com>
2024-08-07 11:01:30 +0530

    scroll added

--- Fashion_App/fashion_app/lib/view/screens/cart_screen.dart
+++ Fashion_App/fashion_app/lib/view/screens/cart_screen.dart
@@ -6,191 +6,193 @@ import 'package:flutter_slidable/flutter_slidable.dart';
 
 import '../../model/fashion_model.dart';
 
-class CartScreen extends StatefulWidget {
+class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
-
-  @override
-  State<CartScreen> createState() => _CartScreenState();
-}
-
-class _CartScreenState extends State<CartScreen> {
-  Row rowFunction(String text, double price) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          text,
-          style: Theme.of(context)
-              .textTheme
-              .bodyMedium!
-              .copyWith(color: const Color.fromRGBO(121, 119, 128, 1)),
-        ),
-        Text("\$$price", style: Theme.of(context).textTheme.bodyLarge),
-      ],
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 30, left: 30, right: 30, bottom: 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              width: MediaQuery.sizeOf(context).width / 2,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: const Icon(Icons.arrow_back_ios),
-                  ),
-                  Text("Cart", style: Theme.of(context).textTheme.bodyLarge),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            Text("My Orders", style: Theme.of(context).textTheme.displayMedium),
-            SizedBox(
-              height: 350,
-              child: ListView.builder(
-                itemCount: fashionItemList.length - 5,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(top: 20),
-                    child: Slidable(
-                      closeOnScroll: true,
-                      endActionPane: ActionPane(
-                        extentRatio: 0.23,
-                        motion: const DrawerMotion(),
-                        children: [
-                          Expanded(
-                            child: Container(
-                              height: 140, // Set height to match Slidable child
-                              padding: const EdgeInsets.all(10),
-                              decoration: const BoxDecoration(
-                                color: AppTheme.primaryLightColor,
-                                borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(40),
-                                  bottomLeft: Radius.circular(40),
-                                ),
-                              ),
-                              alignment: Alignment.center,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  GestureDetector(
-                                    onTap: () {},
-                                    child: const Icon(
-                                      Icons.favorite_border,
-                                      color: Colors.white,
-                                      size: 18,
-                                    ),
-                                  ),
-                                  const SizedBox(width: 10),
-                                  GestureDetector(
-                                    onTap: () {},
-                                    child: const Icon(
-                                      Icons.delete_outline,
-                                      color: Colors.white,
-                                      size: 20,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.sizeOf(context).height,
+          child: Padding(
+            padding: const EdgeInsets.only(
+                top: 30, left: 30, right: 30, bottom: 24), //bottom 30
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.sizeOf(context).width / 2,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: const Icon(Icons.arrow_back_ios),
                       ),
-                      child: Row(
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(30),
-                            child: Image.asset(
-                              fashionItemList[index].imgUrl,
-                              width: 110,
-                              height: 140, // Set height for Slidable child
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          const SizedBox(width: 30),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                      Text("Cart",
+                          style: Theme.of(context).textTheme.bodyLarge),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Text("My Orders",
+                    style: Theme.of(context).textTheme.displayMedium),
+                SizedBox(
+                  height: 350,
+                  child: ListView.builder(
+                    itemCount: fashionItemList.length - 5,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: Slidable(
+                          closeOnScroll: true,
+                          endActionPane: ActionPane(
+                            extentRatio: 0.23,
+                            motion: const DrawerMotion(),
                             children: [
-                              SizedBox(
-                                width: 130,
-                                child: Text(
-                                  "Premium ${fashionItemList[index].brandName}",
-                                  style: Theme.of(context).textTheme.bodyLarge,
+                              Expanded(
+                                child: Container(
+                                  height:
+                                      140, // Set height to match Slidable child
+                                  padding: const EdgeInsets.all(10),
+                                  decoration: const BoxDecoration(
+                                    color: AppTheme.primaryLightColor,
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(40),
+                                      bottomLeft: Radius.circular(40),
+                                    ),
+                                  ),
+                                  alignment: Alignment.center,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      GestureDetector(
+                                        onTap: () {},
+                                        child: const Icon(
+                                          Icons.favorite_border,
+                                          color: Colors.white,
+                                          size: 18,
+                                        ),
+                                      ),
+                                      const SizedBox(width: 10),
+                                      GestureDetector(
+                                        onTap: () {},
+                                        child: const Icon(
+                                          Icons.delete_outline,
+                                          color: Colors.white,
+                                          size: 20,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               ),
-                              Text("Yellow", style: Theme.of(context).textTheme.bodySmall),
-                              Text("Size L", style: Theme.of(context).textTheme.bodySmall),
-                              const SizedBox(height: 5),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            ],
+                          ),
+                          child: Row(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(30),
+                                child: Image.asset(
+                                  fashionItemList[index].imgUrl,
+                                  width: 110,
+                                  height: 140, // Set height for Slidable child
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              const SizedBox(width: 30),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Text(
-                                    "\$${fashionItemList[index].price}",
-                                    style: Theme.of(context).textTheme.titleMedium,
+                                  SizedBox(
+                                    width: 130,
+                                    child: Text(
+                                      "Premium ${fashionItemList[index].brandName}",
+                                      style:
+                                          Theme.of(context).textTheme.bodyLarge,
+                                    ),
                                   ),
-                                  const SizedBox(width: 40),
-                                  Text(
-                                    "1x",
-                                    style: Theme.of(context).textTheme.titleMedium,
+                                  Text("Yellow",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodySmall),
+                                  Text("Size L",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodySmall),
+                                  const SizedBox(height: 5),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        "\$${fashionItemList[index].price}",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .titleMedium,
+                                      ),
+                                      const SizedBox(width: 40),
+                                      Text(
+                                        "1x",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .titleMedium,
+                                      ),
+                                    ],
                                   ),
                                 ],
                               ),
                             ],
                           ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-            // Divider Line
-            const SizedBox(height: 20),
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: const Color.fromRGBO(227, 227, 227, 1),
-                  width: 1,
-                ),
-              ),
-            ),
-            const SizedBox(height: 30),
-            const CustomRowWidget(price: 116.00, text: "Total Items (3)"),
-            const SizedBox(height: 10),
-            const CustomRowWidget(price: 12.00, text: "Standard Delivery"),
-            const SizedBox(height: 10),
-            const CustomRowWidget(price: 126.00, text: "Total Payment"),
-            const Spacer(),
-            Center(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const CheckoutScreen(),
-                    ),
-                  );
-                },
-                child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                  decoration: AppTheme.conDecor,
-                  child: Text(
-                    "Checkout Now",
-                    style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                      );
+                    },
                   ),
                 ),
-              ),
+                // Divider Line
+                const SizedBox(height: 20),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: const Color.fromRGBO(227, 227, 227, 1),
+                      width: 1,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20), //30
+                const CustomRowWidget(price: 116.00, text: "Total Items (3)"),
+                const SizedBox(height: 10),
+                const CustomRowWidget(price: 12.00, text: "Standard Delivery"),
+                const SizedBox(height: 10),
+                const CustomRowWidget(price: 126.00, text: "Total Payment"),
+                const Spacer(),
+                Center(
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => const CheckoutScreen(),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30, vertical: 15),
+                      decoration: AppTheme.conDecor,
+                      child: Text(
+                        "Checkout Now",
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
