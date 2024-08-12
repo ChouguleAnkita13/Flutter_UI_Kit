350088fdf62c048574ded333995b3c6872bf1836
ChouguleAnkita13 <flutterankita@gmail.com>
2024-08-12 15:40:53 +0530

    bottom navigation screen

---
+++ Travel_App/travel_app/lib/view/screens/customnavbar_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CustomNavBarScreen extends StatelessWidget {
+  const CustomNavBarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color.fromRGBO(255, 255, 255, 1),
+    );
+  }
+}
