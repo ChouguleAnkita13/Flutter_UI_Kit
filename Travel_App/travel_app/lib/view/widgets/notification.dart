c3b7188a85d4da65daaf159b2737ddb7b1a455fa
ChouguleAnkita13 <flutterankita@gmail.com>
2024-08-16 12:17:08 +0530

    notification widget

---
+++ Travel_App/travel_app/lib/view/widgets/notification.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class NotificationWidget extends StatelessWidget {
+  const NotificationWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  GestureDetector(
+                      onTap: (){
+                        Get.back();
+                      },
+                      child:  const CircleAvatar(
+                      backgroundColor:  Color.fromRGBO(255, 255, 255, 1),
+                      child: Icon(
+                        Icons.notifications_none_rounded,
+                        size: 25,
+                      ),
+                    ),
+                    );
+  }
+}
