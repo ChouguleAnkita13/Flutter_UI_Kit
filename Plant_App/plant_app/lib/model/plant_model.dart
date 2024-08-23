104bd39296c4496945f17681e3d76406b255d2b0
ChouguleAnkita13 <flutterankita@gmail.com>
2024-08-24 02:10:58 +0530

    model classes for plants

---
+++ Plant_App/plant_app/lib/model/plant_model.dart
@@ -0,0 +1,8 @@
+import 'package:plant_app/model/plantdata_model.dart';
+
+class PlantModel {
+  final String type;
+  final List<PlantdataModel> plantData;
+
+  const PlantModel({required this.type, required this.plantData});
+}
---
+++ Plant_App/plant_app/lib/model/plantdata_model.dart
@@ -0,0 +1,8 @@
+class PlantdataModel{
+  final String plantImg;
+  final String plantName;
+  final int price;
+
+  const PlantdataModel({required this.plantImg, required this.plantName, required this.price});
+
+}
