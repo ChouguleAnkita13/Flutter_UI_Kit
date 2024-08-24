4525f1ad6d8e32a01b59c0e0f49c0c8dc300c300
ChouguleAnkita13 <flutterankita@gmail.com>
2024-08-24 16:20:34 +0530

    done

--- Plant_App/plant_app/lib/main.dart
+++ Plant_App/plant_app/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/controller/plant_controller.dart';
+import 'package:provider/provider.dart';
+import 'package:plant_app/view/get_started_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +12,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return Provider(
+      create:(context)=>PlantController(),
+      child: const MaterialApp(
+        home: GetStarted(),
+        debugShowCheckedModeBanner: false,
       ),
     );
   }
