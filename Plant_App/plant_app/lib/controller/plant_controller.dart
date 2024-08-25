44970555830f13740bfe98a2a0e398be9eb32a40
ChouguleAnkita13 <flutterankita@gmail.com>
2024-08-25 21:30:46 +0530

    added id field

--- Plant_App/plant_app/lib/controller/plant_controller.dart
+++ Plant_App/plant_app/lib/controller/plant_controller.dart
@@ -5,36 +5,44 @@ class PlantController {
   final List<PlantModel> plantList = [
     const PlantModel(type: "Indoor", plantData: [
       PlantdataModel(
+          id: 1,
           plantImg: "assets/images/snakePlant.png",
           plantName: "Snake Plants",
           price: 350),
       PlantdataModel(
+          id: 2,
           plantImg: "assets/images/snakePlant.png",
           plantName: "Snake Plants",
           price: 350),
       PlantdataModel(
+          id: 3,
           plantImg: "assets/images/snakePlant.png",
           plantName: "Snake Plants",
           price: 350),
       PlantdataModel(
+          id: 4,
           plantImg: "assets/images/snakePlant.png",
           plantName: "Snake Plants",
           price: 350),
     ]),
     const PlantModel(type: "Outdoor", plantData: [
       PlantdataModel(
+          id: 5,
           plantImg: "assets/images/snakePlant.png",
           plantName: "Snake Plants",
           price: 350),
       PlantdataModel(
+          id: 6,
           plantImg: "assets/images/snakePlant.png",
           plantName: "Snake Plants",
           price: 350),
       PlantdataModel(
+          id: 7,
           plantImg: "assets/images/snakePlant.png",
           plantName: "Snake Plants",
           price: 350),
       PlantdataModel(
+          id: 8,
           plantImg: "assets/images/snakePlant.png",
           plantName: "Snake Plants",
           price: 350),
--- Plant_App/plant_app/lib/model/plantdata_model.dart
+++ Plant_App/plant_app/lib/model/plantdata_model.dart
@@ -1,8 +1,9 @@
 class PlantdataModel{
+  final int id;// Unique identifier for the item.
   final String plantImg;
   final String plantName;
   final int price;
 
-  const PlantdataModel({required this.plantImg, required this.plantName, required this.price});
+  const PlantdataModel({required this.id,required this.plantImg, required this.plantName, required this.price});
 
 }
