b8d9da2b3f79c06e22b3cc82c83291219c0d6f62
ChouguleAnkita13 <flutterankita@gmail.com>
2024-09-26 15:24:10 +0530

    controllers binding

--- Travel_App/travel_app/lib/main.dart
+++ Travel_App/travel_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:travel_app/controller/controller_binding.dart';
 import 'package:travel_app/controller/getpages_controller.dart';
 
 void main() {
@@ -17,6 +18,10 @@ class MainApp extends StatelessWidget {
 
       /// REGISTERING ALL APPLICATION ROUTES AND THEIR ASSOCIATED SCREENS
       getPages: GetpagesController.getPagesList,
+
+      /// BINDING ALL NECESSARY CONTROLLERS WHEN THE APP STARTS
+
+      initialBinding: ControllerBinding(),
 
       /// DISABLE DEBUG BANNER IN THE TOP RIGHT CORNER
       debugShowCheckedModeBanner: false,
