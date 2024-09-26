3eeb2510240744c955597cb5ab101d74c0b16305
ChouguleAnkita13 <flutterankita@gmail.com>
2024-09-26 12:02:52 +0530

    Getx controller used

--- Travel_App/travel_app/lib/view/screens/customnavbar_screen.dart
+++ Travel_App/travel_app/lib/view/screens/customnavbar_screen.dart
@@ -1,72 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:travel_app/view/screens/HomeScreen/home_screen.dart';
-import 'package:travel_app/view/screens/MessageScreen/messages_screen.dart';
-import 'package:travel_app/view/screens/ProfileScreen/profile_screen.dart';
-import 'package:travel_app/view/screens/ScheduleScreen/schedule_screen.dart';
-import 'package:travel_app/view/screens/SearchScreen/search_screen.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:travel_app/controller/navbar_controller.dart';
 
 ///WIDGET TO DISPLAY CUSTOM NAVIGATIONBAR
-class CustomNavBarScreen extends StatefulWidget {
+class CustomNavBarScreen extends StatelessWidget {
   const CustomNavBarScreen({super.key});
 
   @override
-  State<CustomNavBarScreen> createState() => _CustomNavBarScreenState();
-}
-
-class _CustomNavBarScreenState extends State<CustomNavBarScreen> {
-  int _selectedIndex = 0;
-
-  static final List<Widget> _widgetOptions = [
-    const HomeScreen(),
-    const ScheduleScreen(),
-    const SearchScreen(),
-    const MessagesScreen(),
-    const ProfileScreen(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _widgetOptions.elementAt(_selectedIndex),
-      bottomNavigationBar: SizedBox(
-        height: 100,
-        child: BottomNavigationBar(
-            items: [
-              const BottomNavigationBarItem(
-                  icon: ImageIcon(AssetImage("assets/icons/home.png")),
-                  label: "Home"),
-              const BottomNavigationBarItem(
-                  icon: ImageIcon(AssetImage("assets/icons/calender.png")),
-                  label: "Calender"),
-              BottomNavigationBarItem(
-                  icon: SizedBox(
-                    height: 56,
-                    width: 56,
-                    child: CircleAvatar(
-                      backgroundColor: const Color.fromRGBO(13, 110, 253, 1),
-                      child: Image.asset("assets/icons/search.png"),
+    return GetBuilder(
+      ///INITIALIZE GETX NAVBARCONTROLLER
+      init: NavbarController(),
+      builder: (navbarController) => Scaffold(
+        body: navbarController.widgetOptions
+            .elementAt(navbarController.selectedIndex),
+        bottomNavigationBar: Container(
+          height: 100,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
+          child: BottomNavigationBar(
+              items: [
+                const BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage("assets/icons/home.png")),
+                    label: "Home"),
+                const BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage("assets/icons/calender.png")),
+                    label: "Calender"),
+                BottomNavigationBarItem(
+                    icon: SizedBox(
+                      height: 56,
+                      width: 56,
+                      child: CircleAvatar(
+                        backgroundColor: const Color.fromRGBO(13, 110, 253, 1),
+                        child: Image.asset("assets/icons/search.png"),
+                      ),
                     ),
-                  ),
-                  label: ""),
-              const BottomNavigationBarItem(
-                  icon: ImageIcon(AssetImage("assets/icons/chat.png")),
-                  label: "Messages"),
-              const BottomNavigationBarItem(
-                  icon: ImageIcon(AssetImage("assets/icons/person.png")),
-                  label: "Profile"),
-            ],
-            selectedItemColor: const Color.fromRGBO(13, 110, 253, 1),
-            unselectedItemColor: const Color.fromRGBO(125, 132, 141, 1),
-            backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
-            type: BottomNavigationBarType.fixed,
-            currentIndex: _selectedIndex,
-            onTap: _onItemTapped),
+                    label: ""),
+                const BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage("assets/icons/chat.png")),
+                    label: "Messages"),
+                const BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage("assets/icons/person.png")),
+                    label: "Profile"),
+              ],
+              selectedItemColor: const Color.fromRGBO(13, 110, 253, 1),
+              unselectedItemColor: const Color.fromRGBO(125, 132, 141, 1),
+              backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
+              type: BottomNavigationBarType.fixed,
+              currentIndex: navbarController.selectedIndex,
+              onTap: navbarController.onItemTapped),
+        ),
       ),
     );
   }
